0f2b5861755ec2ed5638c02468f6a944a6794b62
amirreza <amirrezazand@hotmail.com>
2021-11-27 15:50:58 +0330

    Accelo modify

--- iot_Project/urls.py
+++ iot_Project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/User/', include('users.urls')),
+    path('api/Accelo/', include('accelo.urls')),
 ]
