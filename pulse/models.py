5068539cc6db9d60d8be81fefe7f0d8a1d6df17c
amirreza <amirrezazand@hotmail.com>
2021-11-27 18:35:53 +0330

    Create Pulse models

--- pulse/models.py
+++ pulse/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import user
 
 # Create your models here.
+
+class Pulse(models.Model):
+    owner = models.ForeignKey(user , on_delete = models.CASCADE , null = False)
+    Device_ID = models.CharField(max_length=100 , unique=True)
+    Time_send = models.DateTimeField(auto_now=True , null=False)
+    HeartRate = models.CharField(max_length=100 , null = True)
+    Temp = models.CharField(max_length= 100, null= True, blank= True)
+    Auxiliary1 = models.CharField(max_length= 100 , null= True , blank= True)
+    Auxiliary2 = models.CharField(max_length= 100 , null= True , blank= True)
+    Auxiliary3 = models.CharField(max_length= 100 , null= True , blank= True)
+    labeled = models.CharField(max_length=100)
+
+
