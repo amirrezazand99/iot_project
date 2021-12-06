a490f14faa886ad7398d20a00e7b5f0bb78f38e6
amirreza <amirrezazand@hotmail.com>
2021-12-06 19:42:37 +0330

    Create pulse url

--- pulse/urls.py
+++ pulse/urls.py
@@ -6,12 +6,14 @@ from django.contrib import admin
 
 from .views import (
     Pulse_Create_recordAPI,
-    PulseAPI_View
+    PulseAPI_View,
+    PulseFiltered_APIView
 
 )
 
 urlpatterns = [
     url(r'CreateRecord/', Pulse_Create_recordAPI.as_view(), name='create_record'),
     url(r'ViewRecords/', PulseAPI_View.as_view(), name='view_records'),
+    url(r'Filtered/', PulseFiltered_APIView.as_view(), name='filtered_view'),
 
 ]
