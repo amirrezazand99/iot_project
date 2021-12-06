bb3d524a0eddad7a4c25bbaefc1db795eb50817e
amirreza <amirrezazand@hotmail.com>
2021-12-06 19:42:28 +0330

    Create accelo url

--- accelo/urls.py
+++ accelo/urls.py
@@ -5,12 +5,14 @@ from django.contrib import admin
 
 from .views import (
     Accelo_Create_recordAPI,
-    AcceloAPI_View
+    AcceloAPI_View,
+    AcceloFiltered_APIView
 
 )
 
 urlpatterns = [
     url(r'CreateRecord/', Accelo_Create_recordAPI.as_view(), name='create_record'),
     url(r'ViewRecords/', AcceloAPI_View.as_view(), name='view_records'),
+    url(r'Filtered/', AcceloFiltered_APIView.as_view(), name='filtered_view'),
 
 ]
