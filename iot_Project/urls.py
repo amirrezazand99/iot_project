b6939dba6b7954660c8f0b86ad9df77b972f63d6
amirreza <amirrezazand@hotmail.com>
2021-12-11 15:30:28 +0330

    Modify main urls

--- iot_Project/urls.py
+++ iot_Project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+# from rest_framework_swagger.views import get_swagger_view
+# schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    #path('',schema_view),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/User/', include('users.urls')),
