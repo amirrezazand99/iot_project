dc15f5d036bddadc86ccb0b446f548b858d9b2d0
amirreza <amirrezazand@hotmail.com>
2021-12-11 15:30:43 +0330

    Modify pulse's url

--- pulse/urls.py
+++ pulse/urls.py
@@ -11,7 +11,9 @@ from .views import (
 
 )
 
+
 urlpatterns = [
+
     url(r'CreateRecord/', Pulse_Create_recordAPI.as_view(), name='create_record'),
     url(r'ViewRecords/', PulseAPI_View.as_view(), name='view_records'),
     url(r'Filtered/', PulseFiltered_APIView.as_view(), name='filtered_view'),
