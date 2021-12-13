a987ae2ab7ef22f2d9c25e26d2c953ada31266f7
Milad Esfandiyari <miladisjobs@gmail.com>
2021-12-13 12:07:45 +0300

    add make migrations in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 python manage.py collectstatic --noinput
 #python manage.py compilemessages
+python manage.py makemigrations
 python manage.py migrate --noinput
 
 # https://stackoverflow.com/questions/39082768/what-does-set-e-and-exec-do-for-docker-entrypoint-scripts#:~:text=exec%20%22%24%40%22%20is%20typically%20used,to%20the%20command%20line%20arguments.
