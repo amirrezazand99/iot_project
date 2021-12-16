d8645f18885cf44de5a4390ab6929c17aa8e85c6
amirreza <amirrezazand@hotmail.com>
2021-12-16 15:32:21 +0330

    edit django admin panel

--- devices/admin.py
+++ devices/admin.py
@@ -1,9 +1,11 @@
-# from django.contrib import admin
-# from .models import devices
-# from django.contrib.admin import ModelAdmin
-#
-#
-# # Register your models here.
+from django.contrib import admin
+from .models import devices
+from django.contrib.admin import ModelAdmin
+
+
+
+admin.register(devices)
+# Register your models here.
 # @admin.register(devices)
 # class Pulse(ModelAdmin):
 #     fields = '__all__'
